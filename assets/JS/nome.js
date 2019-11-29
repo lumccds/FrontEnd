b1c9955e0cbead6430cb09ecb66892fc83006004
Turma Generation <lumccds@gmail.com>
2019-11-29 14:49:33 -0300

    JS LIST

---
+++ assets/JS/nome.js
@@ -0,0 +1,13 @@
+function verNome(){
+    var nome = document.getElementById("nome").value;
+    
+}
+
+
+
+str.slice(0, " ");
+
+prompt("Qual o seu nome completo?");
+
+console.log(nome);
+
---
+++ assets/JS/to-do-list.js
@@ -0,0 +1,6 @@
+var checado
+
+function novaTarefa(idInput){
+    var novaTarefa = document.getElementById("novaTarefa").value;
+    var novaLinha = '<input class="form-check-input" type="checkbox"><label class="form-check-label"></label>';
+}
