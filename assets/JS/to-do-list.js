38a87b94ef948df1c575a9b84e61142909235038
JoaoLimaBRR <lumccds@gmail.com>
2019-12-02 16:01:08 -0300

    JS 02-12

--- assets/JS/to-do-list.js
+++ assets/JS/to-do-list.js
@@ -1,6 +1,25 @@
-var checado
-
-function novaTarefa(idInput){
-    var novaTarefa = document.getElementById("novaTarefa").value;
-    var novaLinha = '<input class="form-check-input" type="checkbox"><label class="form-check-label"></label>';
-}
+function novaTarefa() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("novaTarefa").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("Você precisa escrever algo!");
+    } else {
+      document.getElementById("lista").appendChild(li);
+    }
+    document.getElementById("novaTarefa").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
